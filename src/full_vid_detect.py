1f01f3a6cfabd535ce08e71befe6f3177c310842
Dr-Vphuc <ngphuc689@gmail.com>
2025-11-25 02:02:28 +0700

    add full_vid detect

---
+++ src/full_vid_detect.py
@@ -0,0 +1,127 @@
+import os
+os.environ["CORE_MODEL_SAM_ENABLED"] = "False"
+os.environ["CORE_MODEL_SAM2_ENABLED"] = "False"
+os.environ["CORE_MODEL_SAM3_ENABLED"] = "False"
+os.environ["CORE_MODEL_GAZE_ENABLED"] = "False"
+os.environ["CORE_MODEL_GROUNDINGDINO_ENABLED"] = "False"
+os.environ["CORE_MODEL_YOLO_WORLD_ENABLED"] = "False"
+os.environ["ONNXRUNTIME_EXECUTION_PROVIDERS"] = "[CUDAExecutionProvider]"
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from inference import get_model
+import supervision as sv
+import numpy as np
+import torch
+from libs.func import resolve_goalkeepers_team_id, draw_pitch, draw_pitch_voronoi_diagram_2
+from libs.team import TeamClassifier
+from libs.view import ViewTransformer
+from libs.configs import SoccerPitchConfiguration
+from libs.annotators import draw_points_on_pitch, draw_pitch_voronoi_diagram
+import cv2
+
+from dotenv import load_dotenv
+load_dotenv()
+import importlib
+_inf = importlib.import_module('inference')
+ROBOFLOW_API_KEY = os.getenv("ROBOFLOW_API_KEY") or getattr(_inf, 'API_KEY', None)
+
+PLAYER_DETECTION_MODEL_ID = "football-players-detection-3zvbc/11"
+PLAYER_DETECTION_MODEL = get_model(model_id=PLAYER_DETECTION_MODEL_ID, api_key=ROBOFLOW_API_KEY)
+
+SOURCE_VIDEO_PATH = '/home/vphuc/Project/AI/Football_Analysis/121364_0.mp4'
+OUTPUT_VIDEO_PATH = '/home/vphuc/Project/AI/Football_Analysis/output_121364_0.mp4'
+BALL_ID = 0
+GOALKEEPER_ID = 1
+PLAYER_ID = 2
+REFEREE_ID = 3
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+BATCH_SIZE = 32
+CONFIG = SoccerPitchConfiguration()
+CAP = cv2.VideoCapture(SOURCE_VIDEO_PATH)
+WIDTH  = int(CAP.get(cv2.CAP_PROP_FRAME_WIDTH))
+HEIGHT = int(CAP.get(cv2.CAP_PROP_FRAME_HEIGHT))
+FPS    = CAP.get(cv2.CAP_PROP_FPS)
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(OUTPUT_VIDEO_PATH, fourcc, FPS, (WIDTH, HEIGHT))
+
+ellipse_annotator = sv.EllipseAnnotator(
+    color=sv.ColorPalette.from_hex(['#00BFFF', '#FF1493', '#FFD700']),
+    thickness=2
+)
+label_annotator = sv.LabelAnnotator(
+    color=sv.ColorPalette.from_hex(['#00BFFF', '#FF1493', '#FFD700']),
+    text_color=sv.Color.from_hex('#000000'),
+    text_position=sv.Position.BOTTOM_CENTER
+)
+triangle_annotator = sv.TriangleAnnotator(
+    color=sv.Color.from_hex('#FFD700'),
+    base=20, height=17
+)
+
+tracker = sv.ByteTrack()
+tracker.reset()
+
+frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+
+for frame in frame_generator:
+    # ball, goalkeeper, player, referee detection
+
+    result = PLAYER_DETECTION_MODEL.infer(frame, confidence=0.3)[0]
+    detections = sv.Detections.from_inference(result)
+
+    ball_detections = detections[detections.class_id == BALL_ID]
+    ball_detections.xyxy = sv.pad_boxes(xyxy=ball_detections.xyxy, px=10)
+
+    all_detections = detections[detections.class_id != BALL_ID]
+    all_detections = all_detections.with_nms(threshold=0.5, class_agnostic=True)
+    all_detections = tracker.update_with_detections(detections=all_detections)
+
+    goalkeepers_detections = all_detections[all_detections.class_id == GOALKEEPER_ID]
+    players_detections = all_detections[all_detections.class_id == PLAYER_ID]
+    referees_detections = all_detections[all_detections.class_id == REFEREE_ID]
+
+    # team assignment
+
+    team_classifier = TeamClassifier(DEVICE, BATCH_SIZE)
+    players_crops = [sv.crop_image(frame, xyxy) for xyxy in players_detections.xyxy]
+    team_classifier.fit(players_crops)
+    players_detections.class_id = team_classifier.predict(players_crops)
+
+    goalkeepers_detections.class_id = resolve_goalkeepers_team_id(
+        players_detections, goalkeepers_detections)
+
+    referees_detections.class_id -= 1
+
+    all_detections = sv.Detections.merge([
+        players_detections, goalkeepers_detections, referees_detections])
+
+
+    # frame visualization
+
+    labels = [
+        f"#{tracker_id}"
+        for tracker_id
+        in all_detections.tracker_id
+    ]
+
+    all_detections.class_id = all_detections.class_id.astype(int)
+
+    annotated_frame = frame.copy()
+    annotated_frame = ellipse_annotator.annotate(
+        scene=annotated_frame,
+        detections=all_detections)
+    annotated_frame = label_annotator.annotate(
+        scene=annotated_frame,
+        detections=all_detections,
+        labels=labels)
+    annotated_frame = triangle_annotator.annotate(
+        scene=annotated_frame,
+        detections=ball_detections)
+
+    out.write(annotated_frame)
+
+out.release()
+CAP.release()
